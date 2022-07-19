7f5f545351bfeb667e0fe2fe52c3b59e33761ba4
rikiyaota <rikiya0421.fcb6@gmail.com>
2022-07-19 09:20:45 +0900

    Add a new functionality to CountDisplay class.

--- src/bridgePattern/countDisplay.ts
+++ src/bridgePattern/countDisplay.ts
@@ -11,4 +11,9 @@ export class CountDisplay extends Display {
     for (let i = 0; i < times; i++) this.print();
     this.close();
   }
+
+  randomDisplay(max: number) {
+    const times = Math.floor(Math.random() * max);
+    this.multiDisplay(times);
+  }
 }
