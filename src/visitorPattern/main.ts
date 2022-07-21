c2318507bec03dbc0b55d5f34708706cac18edaf
rikiyaota <rikiya0421.fcb6@gmail.com>
2022-07-21 14:43:30 +0900

    Add more statements to demo script of Visitor Pattern.

--- src/visitorPattern/main.ts
+++ src/visitorPattern/main.ts
@@ -1,6 +1,7 @@
 import { Directory } from "./directory";
 import { File } from "./file";
 import { ListVisitor } from "./listVisitor";
+import { FileFindVisitor } from "./fileFindVisitor";
 
 console.log("Making root entries ...");
 
@@ -32,7 +33,20 @@ usrDir.add(hanakoDir);
 rikiyaDir.add(new File("diary.html", 100));
 rikiyaDir.add(new File("Composite.java", 200));
 taroDir.add(new File("memo.tex", 300));
+taroDir.add(new File("index.html", 300));
 hanakoDir.add(new File("game.doc", 400));
 hanakoDir.add(new File("junk.mail", 500));
 
 rootDir.accept(new ListVisitor());
+
+console.log();
+console.log("Listing only HTML files...");
+const ffv = new FileFindVisitor("html");
+rootDir.accept(ffv);
+const iterator = ffv.getFoundFiles();
+let item = iterator.next();
+while (!item.done) {
+  const file = item.value;
+  console.log(file.toString());
+  item = iterator.next();
+}
